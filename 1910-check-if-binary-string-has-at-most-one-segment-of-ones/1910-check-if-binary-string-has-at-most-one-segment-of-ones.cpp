1a0dfd9bb84b505846fde8058bbab40fb775932a
bitopan baruah <bitopanbaruah222@gmail.com>
2026-07-05 00:53:06 +0530

    1910-CHECK-IF-BINARY-STRING-HAS-AT-MOST-ONE-SEGMENT-OF-ONES Stats: Time: 0 ms (100%), Space: 8.2 MB (46.1%) - LeetHub

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/1910-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/1910-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        int count=1;
-
         for(int i=1;i<s.length();i++){
-            if(s[i]=='1'){
-                count++;
-            }
-            if(s[i]==s[i-1]&&s[i]=='1'){
-                count--;
-
+            if(s[i]=='1'&&s[i-1]=='0'){
+                return false;
             }
         }
-        if(count==1){
-            return true;
-        }
-        return false;
+        return true;
     }
 };
