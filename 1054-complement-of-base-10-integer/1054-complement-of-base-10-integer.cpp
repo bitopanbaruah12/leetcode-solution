95d0526ed892f396a075a4750e34490728a8b6dd
bitopan baruah <bitopanbaruah222@gmail.com>
2026-07-05 22:58:23 +0530

    1054-COMPLEMENT-OF-BASE-10-INTEGER Stats: Time: 0 ms (100%), Space: 7.7 MB (92.11%) - LeetHub

--- 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        vector<int> binary;
+        int multi=1;
+        int total=0;
         if(n==0){
             return 1;
         }
-        while (n > 0) {
-            binary.push_back(n % 2);
-            n = n / 2;
-        }
-        for (int i = 0; i < binary.size(); i++) {
-            if (binary[i] == 1) {
-                binary[i] = 0;
-            } else {
-                binary[i] = 1;
-            }
-        }
-        int multi = 1;
-        int total = 0;
+        while(n>0){
+            int reminder=n%2;
+            if(reminder==1){
+                reminder=0;
+                total=total+(reminder)*multi;
 
-        for (int i = 0; i < binary.size(); i++) {
-            total = total + binary[i] * multi;
-            multi = multi * 2;
+            }
+            else{
+                reminder=1;
+                total=total+reminder*multi;
+            }
+            n/=2;
+            multi*=2;
         }
         return total;
     }
