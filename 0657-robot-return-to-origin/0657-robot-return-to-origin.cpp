000340c86a54488417cdce41d001dedd8c565152
bitopan baruah <bitopanbaruah222@gmail.com>
2026-07-09 00:01:33 +0530

    0657-ROBOT-RETURN-TO-ORIGIN Stats: Time: 2 ms (52.66%), Space: 10.4 MB (46.27%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        int count_R=0;
-        int count_L=0;
-        int count_U=0;
-        int count_D=0;
+        int x=0;
+        int y=0;
         for(int i=0;i<moves.length();i++){
             if(moves[i]=='R'){
-                count_R++;
+                x++;
             }
             if(moves[i]=='L'){
-                count_L++;
+                x--;
             }
             if(moves[i]=='U'){
-                count_U++;
+                y++;
             }
             if(moves[i]=='D'){
-                count_D++;
+                y--;
             }
 
         }
-        if(count_R==count_L&&count_U==count_D){
+        if(x==0&&y==0){
             return true;
 
         }
