e6770b309d7bda586f711d5cd56e5931b2cc14b8
bitopan baruah <bitopanbaruah222@gmail.com>
2026-07-18 12:00:22 +0530

    Time: 0 ms (100%), Space: 14.1 MB (88.16%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
@@ -5,19 +5,20 @@ public:
         
         
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
+            int x =nums[i];
+            if (x == 0) {
                 answer.push_back(0);
+                continue;
             }
-            int n=nums[i];
-            vector<int> support;
-            while(n>0){
-                support.push_back(n%10);
-                n/=10;
 
-            }
-            for(int j=support.size()-1;j>=0;j--){
-                answer.push_back(support[j]);
+            int div = 1;
+            while (x / div >= 10)
+                div *= 10;
 
+            while (div > 0) {
+                answer.push_back(x / div);
+                x %= div;
+                div /= 10;
             }
             
         }
