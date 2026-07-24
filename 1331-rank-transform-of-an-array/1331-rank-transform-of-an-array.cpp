a1f780ab4c7d0a50232c1af6798704daf36c2850
bitopan baruah <bitopanbaruah222@gmail.com>
2026-07-24 11:58:34 +0530

    Time: 75 ms (15.82%), Space: 43.4 MB (70.89%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        vector<int> ans=arr;
-        sort(arr.begin(),arr.end());
-        map<int,int> rank;
-        int r =1;
-        for(int i=0;i<arr.size();i++){
-            if(rank.find(arr[i])==rank.end()){
-                rank[arr[i]]=r;
+        vector<int> ans = arr;
+        sort(arr.begin(), arr.end());
+        map<int, int> rank;
+        int r = 1;
+        for (int i = 0; i < arr.size(); i++) {
+            if (rank.find(arr[i]) == rank.end()) {
+                rank[arr[i]] = r;
                 r++;
             }
         }
-        for(int i=0;i<ans.size();i++){
-            ans[i]=rank[ans[i]];
+        for (int i = 0; i < ans.size(); i++) {
+            ans[i] = rank[ans[i]];
         }
         return ans;
     }
