af63a68a649dded3e575f22bed7f52498df87c26
bitopan baruah <bitopanbaruah222@gmail.com>
2026-07-24 12:00:00 +0530

    Time: 34 ms (76.77%), Space: 43 MB (75.38%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         vector<int> ans = arr;
         sort(arr.begin(), arr.end());
-        map<int, int> rank;
+        unordered_map<int, int> rank;
         int r = 1;
         for (int i = 0; i < arr.size(); i++) {
             if (rank.find(arr[i]) == rank.end()) {
