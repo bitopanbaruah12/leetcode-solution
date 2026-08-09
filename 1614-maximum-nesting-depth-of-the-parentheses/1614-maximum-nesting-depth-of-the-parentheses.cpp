70c51fad5654abe87e6baf522b73ac576147d641
bitopan baruah <bitopanbaruah222@gmail.com>
2026-08-09 11:50:15 +0530

    Time: 0 ms (100%), Space: 8.3 MB (86.88%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth=0;
+        int ans=0;
+
+        for(int i=0;i<s.length();i++ ){
+            if(s[i]=='('){
+                depth++;
+                ans=max(ans,depth);
+            }
+            else if(s[i]==')'){
+                depth--;
+
+            }
+           
+        }
+        return ans;
+    }
+};
