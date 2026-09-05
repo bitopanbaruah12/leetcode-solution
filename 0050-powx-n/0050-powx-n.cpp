69700f1ef229c1e6a9499610ede1f1e50207c0b8
bitopan baruah <bitopanbaruah222@gmail.com>
2026-09-05 21:44:56 +0530

    Time: 0 ms (100%), Space: 8.8 MB (13.79%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
+    double pow(double x, int n){
+        if(n==0){
+            return 1;
+        }
+        double half=pow(x,n/2);
+
+        if(n%2==0){
+            return half*half;
+        }
+        else{
+            return x*half*half;
+        }
+
+    }
     double myPow(double x, int n) {
-        return pow(x,n);
+        long long N=n;
+        if(n<0){
+            return 1/pow(x,-N);
+        }
+        else{
+            return pow(x,N);
+        }
     }
 };
